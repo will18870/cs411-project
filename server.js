dd7a1f7fb03d578267494cb1cedc6a844f5215c4
wh <wh@cs350-gitlab.bu.edu>
2023-04-04 18:42:08 -0400

    made call with ticketmaster

--- server.js
+++ server.js
@@ -3,12 +3,21 @@
 const http = require('http');
 const express = require('express')
 const app = express()
+const request = require('request')
 
 const hostname = '127.0.0.1';
 const port = 3000;
+const tmKey = '0txRTCElnYysjp6wGw85pTQwcXUPIXfv';
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    request('https://app.ticketmaster.com/discovery/v2/events.json?postalCode=02215&apikey=0txRTCElnYysjp6wGw85pTQwcXUPIXfv', function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            res.send(body) 
+        }
+        else {
+            res.send('Hello World - google not work :(')
+        }
+    })
 })
 
 app.get('/test', (req, res) => {
