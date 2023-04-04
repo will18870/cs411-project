e14b27b8f30c09b2be34d9883b64377f1f48aea5
wh <wh@cs350-gitlab.bu.edu>
2023-04-04 18:02:12 -0400

    express server test

--- server.js
+++ server.js
@@ -1,16 +1,20 @@
 // basic server test
 
 const http = require('http');
+const express = require('express')
+const app = express()
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World');
-});
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
 
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
-});
+app.get('/test', (req, res) => {
+    res.send('Hello World from Test')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
