352d26a1ff95a80a82dc32f52d177bff3ddfacc2
wh <wh@cs350-gitlab.bu.edu>
2023-04-04 18:50:36 -0400

    received and parsed json

--- server.js
+++ server.js
@@ -12,7 +12,9 @@ const tmKey = '0txRTCElnYysjp6wGw85pTQwcXUPIXfv';
 app.get('/', (req, res) => {
     request('https://app.ticketmaster.com/discovery/v2/events.json?postalCode=02215&apikey=0txRTCElnYysjp6wGw85pTQwcXUPIXfv', function (error, response, body) {
         if (!error && response.statusCode == 200) {
-            res.send(body) 
+            info = JSON.parse(body)
+            console.log(info)
+            res.send(info._embedded.events)
         }
         else {
             res.send('Hello World - google not work :(')
