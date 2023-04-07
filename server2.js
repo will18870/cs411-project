1ce2d6a32348ee62ee57950846b1a78f47b48565
wh <wh@bu.edu>
2023-04-07 01:27:18 -0400

    prototype zip searching works!

--- server2.js
+++ server2.js
@@ -2,35 +2,42 @@ const express = require('express');
 const app = express();
 const request = require('request')
 const port = 3001;
+const path = require("path");
+const bodyParser = require('body-parser')
 
+app.use(bodyParser())
 app.get('/', (req, res) => {
-    res.send(`
-      <form action="/result" method="get">
-        <input type="text" name="location" placeholder="Enter location">
-        <button type="submit">Search</button>
-      </form>
-    `);
+    res.sendFile(path.join(__dirname,'index.html'));
  });
 
  const tmKey = '0txRTCElnYysjp6wGw85pTQwcXUPIXfv';
 
- app.get('/result', (req, res) => {
-     request('https://app.ticketmaster.com/discovery/v2/events.json?postalCode=02215&apikey=0txRTCElnYysjp6wGw85pTQwcXUPIXfv', function (error, response, body) {
+ app.post('/result', (req, res) => {
+
+
+     const zip = req.body.zip
+     request('https://app.ticketmaster.com/discovery/v2/events.json?postalCode=' + zip + '&apikey=' + tmKey, function (error, response, body) {
          if (!error && response.statusCode == 200) {
              info = JSON.parse(body)
+             console.log(info)
+             if (info.page.totalElements == 0)
+             {
+                 res.send('no concerts at this zip code :(')
+                 return
+             }
              eventList = info._embedded.events
              console.log(info)
              //res.send(info._embedded.events)
- 
+
              result = ""
              for(let i = 0; i < eventList.length; i++) {
-                 result += eventList[i].name + "\n";
+                 result += eventList[i].name + "<br>";
              }
- 
+
              res.send(result)
          }
          else {
-             res.send('Hello World - google not work :(')
+             res.send('Error: Request did not go through :(')
          }
      })
 
