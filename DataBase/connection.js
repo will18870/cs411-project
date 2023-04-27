8dd67cfa6cb8a116fd658ece6edccf47345f859e
MariaEusseHenao <112965558+MariaEusseHenao@users.noreply.github.com>
2023-04-27 13:26:36 -0400

    Add files via upload
    get_savedConcert_fuction working

--- DataBase/connection.js
+++ DataBase/connection.js
@@ -1,7 +1,6 @@
 const { MongoClient } = require('mongodb');
-//const { deleteConcertFromUser } = require('./delete_concert_function');
-
 const testMap = {};
+
 testMap["Albert"] = [
   {
     "id": 1,
@@ -11,7 +10,7 @@ testMap["Albert"] = [
     "location": "Boston",
     "ticketLink": "https://example.com/tickets/1",
     "description": "This is the description of Concert 1.",
-    "genre": "Pop",
+    "genre": "Pop"
   },
   {
     "id": 2,
@@ -21,14 +20,11 @@ testMap["Albert"] = [
     "location": "New York",
     "ticketLink": "https://example.com/tickets/2",
     "description": "This is the description of Concert 2.",
-    "genre": "Rock"}]
+    "genre": "Rock"
+  }
+];
 
-console.log(testMap["Albert"][0]);
-const arr = Object.entries(testMap["Albert"][0])
-console.log(arr);
-
-
-async function main(){
+async function main() {
   const uri = "mongodb+srv://acheng1:123albert123cheng@cluster0.bpmkkw8.mongodb.net/?retryWrites=true&w=majority";
   const client = new MongoClient(uri);
 
@@ -42,69 +38,48 @@ async function main(){
       concerts: testMap["Albert"]
     });*/
 
-} catch (e) {
-    console.error(e)
+    // Add a concert for a user
+    const { addConcertToUser } = require('./add_concert_function');
+    // example manually calling add_concert_function test
+    await addConcertToUser('Albert', {
+      id: 2,
+      title: "Queen",
+      image: "3.jpg",
+      date: "2024-09-01",
+      location: "Boston",
+      ticketLink: "https://example.com/tickets/3",
+      description: "This is the description of Concert 3.",
+      genre: "Rock"
+    });
 
-} finally {
+    // Delete a concert from a user
+    const { deleteConcertFromUser } = require('./delete_concert_function');
+    // example manually calling delete_concert_function test
+    deleteConcertFromUser('Albert', 'Adele');
+
+    // Retrieve user's saved concerts given the user key 'name'
+    const { getConcertTitlesByUserName } = require('./get_savedConcerts_function.js');
+    const concertTitles = await getConcertTitlesByUserName('Albert');
+    console.log(concertTitles);
+  } catch (e) {
+    console.error(e);
+  } finally {
     await client.close();
+  }
 }
 
 
 
 
-// Add a concert for a user
-const { addConcertToUser } = require('./add_concert_function');
-// example manually calling add_concert_function test
-await addConcertToUser('Albert', {
-    id: 2,
-    title: "Queen",
-    image: "3.jpg",
-    date: "2024-09-01",
-    location: "Boston",
-    ticketLink: "https://example.com/tickets/3",
-    description: "This is the description of Concert 3.",
-    genre: "Rock",
-});
-
-
-// Delete a concert from an user
-const { deleteConcertFromUser } = require('./delete_concert_function');
-// example manually calling delte_concert_function test
-deleteConcertFromUser('Albert', 'Adele');
-
-
-//Retreive user's saved concerts given the user key 'name'
-//example manually calling getConcertTitlesByUserName test
-const { getConcertTitlesByUserName } = require('./get_savedConcerts_function.js');
-
-async function printConcertTitles() {
-  const concertTitles = await getConcertTitlesByUserName('Albert');
-  console.log(concertTitles);
+async function listDatabases(client) {
+  const databasesList = await client.db().admin().listDatabases();
+  console.log("Databases:");
+  databasesList.databases.forEach(db => console.log(` - ${db.name}`));
 }
 
-printConcertTitles();
-
-
-
-    
-
-
+async function storeUsers(client, newListing) {
+  const result = await client.db("Users").collection("Users_Concert").insertOne(newListing);
+  console.log(`new listing created: ${result.insertedID}`);
 }
+
 main().catch(console.error);
-
-async function listDatabases(client){
-    databasesList = await client.db().admin().listDatabases();
-
-    console.log("Databases:");
-    databasesList.databases.forEach(db => console.log(` - ${db.name}`));
-};
- 
-
-async function storeUsers(client,newListing){
-   const result = await client.db("Users").collection("Users_Concert").insertOne(newListing);
-   console.log(`new listing created: ${result.insertedID}`);
-}
-
-//console.log(myMap["Albert"]);
-//console.log(testMap["Albert"]);
-
--- DataBase/get_savedConcerts_function.js
+++ DataBase/get_savedConcerts_function.js
@@ -1,21 +1,30 @@
 const { MongoClient } = require('mongodb');
 
 async function getConcertTitlesByUserName(userName) {
-    const uri = "mongodb+srv://acheng1:123albert123cheng@cluster0.bpmkkw8.mongodb.net/?retryWrites=true&w=majority";
-    const client = new MongoClient(uri);
-  
-    function getConcertTitlesByUserName(userName) {
-        const userConcerts = testMap[userName];
-        const concertTitles = [];
-      
-        for (let i = 0; i < userConcerts.length; i++) {
-          const concert = userConcerts[i];
-          concertTitles.push(concert.title);
-        }
-      
-        return concertTitles;
-      }
-    }      
+  const uri = "mongodb+srv://acheng1:123albert123cheng@cluster0.bpmkkw8.mongodb.net/?retryWrites=true&w=majority";
+  const client = new MongoClient(uri);
+
+
+
+  try {
+    await client.connect();
+    const database = client.db("Users");
+    const collection = database.collection("Users_Concert");
+
+    const userConcerts = await collection.findOne({ name: userName });
+
+    if (userConcerts && userConcerts.concert) {
+      const concertTitles = userConcerts.concert.map(concert => concert.title);
+      return concertTitles;
+    }
+
+    return [];
+  } catch (e) {
+    console.error(e);
+    return [];
+  } finally {
+    await client.close();
+  }
+}
 
 module.exports = { getConcertTitlesByUserName };
-  
